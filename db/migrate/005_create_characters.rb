afd0957dfb9f8d4886a8f8bab3096d6a4160e7cf
Min Kim <mk4555@nyu.edu>
2018-01-11 17:22:46 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
-    create_table :chracters do |t|
+    create_table :characters do |t|
       t.string :name
       t.integer :show_id
     end
